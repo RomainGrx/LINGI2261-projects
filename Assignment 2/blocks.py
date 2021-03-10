656a7fb04716adf915eba113dc19177d4ca67df2
Romain Graux <romain.graux@student.uclouvain.be>
2021-03-10 10:48:54 +0100

    First version of the problem Blocks

--- Assignment 2/blocks.py
+++ Assignment 2/blocks.py
@@ -1,5 +1,19 @@
-# -*-coding: utf-8 -*
-'''NAMES OF THE AUTHOR(S): Gaël Aglin <gael.aglin@uclouvain.be>'''
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author : Romain Graux
+@date : 2021 Mar 10, 09:20:31
+@last modified : 2021 Mar 10, 10:47:36
+"""
+
+"""NAMES OF THE AUTHOR(S): Gael Aglin <gael.aglin@uclouvain.be>
+                           Vincent Buccilli <vincent.buccilli@student.uclouvain.be>
+                           Romain Graux <romain.graux@student.uclouvain.be>"""
+
+INGINIOUS = False
+MYTEST = True
+
+import numpy as np
 from search import *
 import sys
 import time
@@ -9,9 +23,45 @@ goal_state = None
 # Problem class #
 #################
 class Blocks(Problem):
+    WALL = "#"
+    VOID = " "
+    AVAILABLE_MOVES = [(0, -1), (0, 1)]  # i.e. LEFT, RIGHT
+
+    @staticmethod
+    def valid_position(state, pos):
+        y, x = pos
+        return (
+            0 <= y < state.nbr
+            and 0 <= x < state.nbc
+            and state.grid[y, x] == Blocks.VOID
+        )
+
+    @staticmethod
+    def apply_gravity(state, pos):
+        def have_support(state, pos):
+            y, x = pos
+            return y == (state.nbr - 1) or state.grid[y + 1, x] == Blocks.WALL
+
+        y, x = pos
+        while not have_support(state, (y, x)):
+            y += 1
+        return (y, x)
 
     def successor(self, state):
-        pass
+        """successor.
+        return the successor (i.e. all possible moves for each block instance)
+        - identify position of all blocks (return only blocks not touching a
+          goal position)
+        - try all possible moves :: check validity + apply gravity
+
+        :param state:
+        """
+        for block_id, (y, x) in enumerate(state.blocks):
+            for dy, dx in Blocks.AVAILABLE_MOVES:
+                new_pos = y + dy, x + dx
+                if Blocks.valid_position(state, new_pos):
+                    new_pos = Blocks.apply_gravity(state, new_pos)
+                    yield 0, state.new_state(block_id, new_pos)
 
     def goal_test(self, state):
         pass
@@ -24,7 +74,23 @@ class State:
     def __init__(self, grid):
         self.nbr = len(grid)
         self.nbc = len(grid[0])
-        self.grid = grid
+        self.grid = np.array(grid)
+        self.blocks = np.array(
+            np.argwhere(
+                np.logical_and(self.grid != Blocks.WALL, self.grid != Blocks.VOID)
+            )
+        )
+
+    def new_state(self, block_id, new_pos):
+        y, x = new_pos
+        prev_y, prev_x = self.blocks[block_id]
+        cls = self.grid[prev_y, prev_x]
+        new_grid = self.grid.copy()
+        # if new_grid[y, x] != Blocks.VOID:
+        #     raise Exception(f"Not void position for {y, x} :: {new_grid[y, x]}")
+        new_grid[prev_y, prev_x] = Blocks.VOID
+        new_grid[y, x] = cls
+        return State(new_grid)
 
     def __str__(self):
         n_sharp = self.nbc + 2
@@ -35,7 +101,7 @@ class State:
                 s = s + str(self.grid[i][j])
             s += "#"
             if i < self.nbr - 1:
-                s += '\n'
+                s += "\n"
         return s + "\n" + "#" * n_sharp
 
 
@@ -43,8 +109,12 @@ class State:
 # Auxiliary function #
 ######################
 def readInstanceFile(filename):
-    grid_init, grid_goal = map(lambda x: [[c for c in l.rstrip('\n')[1:-1]] for l in open(filename + x)], [".init", ".goalinfo"])
+    grid_init, grid_goal = map(
+        lambda x: [[c for c in l.rstrip("\n")[1:-1]] for l in open(filename + x)],
+        [".init", ".goalinfo"],
+    )
     return grid_init[1:-1], grid_goal[1:-1]
+
 
 ######################
 # Heuristic function #
@@ -56,64 +126,108 @@ def heuristic(node):
     # ...
     return h
 
+
 ##############################
 # Launch the search in local #
 ##############################
-#Use this block to test your code in local
+# Use this block to test your code in local
 # Comment it and uncomment the next one if you want to submit your code on INGInious
-'''instances_path = "instances/"
-instance_names = ['a01','a02','a03','a04','a05','a06','a07','a08','a09','a10','a11']
 
-for instance in [instances_path + name for name in instance_names]:
-    grid_init, grid_goal = readInstanceFile(instance)
-    init_state = State(grid_init)
-    goal_state = State(grid_goal)
-    problem = Blocks(init_state)
+if __name__ == "__main__":
 
-    # example of bfs tree search
-    startTime = time.perf_counter()
-    node, nb_explored, remaining_nodes = depth_first_graph_search(problem)
-    endTime = time.perf_counter()
+    if not INGINIOUS and not MYTEST:
+        instances_path = "instances/"
+        instance_names = [
+            "a01",
+            "a02",
+            "a03",
+            "a04",
+            "a05",
+            "a06",
+            "a07",
+            "a08",
+            "a09",
+            "a10",
+            "a11",
+        ]
 
-    # example of print
-    path = node.path()
-    path.reverse()
+        for instance in [instances_path + name for name in instance_names]:
+            grid_init, grid_goal = readInstanceFile(instance)
+            init_state = State(grid_init)
+            goal_state = State(grid_goal)
+            problem = Blocks(init_state)
 
-    print('Number of moves: ' + str(node.depth))
-    for n in path:
-        print(n.state)  # assuming that the __str__ function of state outputs the correct format
-        print()
-    print("* Execution time:\t", str(endTime - startTime))
-    print("* Path cost to goal:\t", node.depth, "moves")
-    print("* #Nodes explored:\t", nb_explored)
-    print("* Queue size at goal:\t",  remaining_nodes)'''
+            # example of bfs tree search
+            startTime = time.perf_counter()
+            node, nb_explored, remaining_nodes = depth_first_graph_search(problem)
+            endTime = time.perf_counter()
 
+            # example of print
+            path = node.path()
+            path.reverse()
 
+            print("Number of moves: " + str(node.depth))
+            for n in path:
+                print(
+                    n.state
+                )  # assuming that the __str__ function of state outputs the correct format
+                print()
+            print("* Execution time:\t", str(endTime - startTime))
+            print("* Path cost to goal:\t", node.depth, "moves")
+            print("* #Nodes explored:\t", nb_explored)
+            print("* Queue size at goal:\t", remaining_nodes)
+    elif INGINIOUS and not MYTEST:
 
-####################################
-# Launch the search for INGInious  #
-####################################
-#Use this block to test your code on INGInious
-instance = sys.argv[1]
-grid_init, grid_goal = readInstanceFile(instance)
-init_state = State(grid_init)
-goal_state = State(grid_goal)
-problem = Blocks(init_state)
+        ####################################
+        # Launch the search for INGInious  #
+        ####################################
+        # Use this block to test your code on INGInious
+        instance = sys.argv[1]
+        grid_init, grid_goal = readInstanceFile(instance)
+        init_state = State(grid_init)
+        goal_state = State(grid_goal)
+        problem = Blocks(init_state)
 
-# example of bfs graph search
-startTime = time.perf_counter()
-node, nb_explored, remaining_nodes = astar_graph_search(problem, heuristic)
-endTime = time.perf_counter()
+        # example of bfs graph search
+        startTime = time.perf_counter()
+        node, nb_explored, remaining_nodes = astar_graph_search(problem, heuristic)
+        endTime = time.perf_counter()
 
-# example of print
-path = node.path()
-path.reverse()
+        # example of print
+        path = node.path()
+        path.reverse()
 
-print('Number of moves: ' + str(node.depth))
-for n in path:
-    print(n.state)  # assuming that the __str__ function of state outputs the correct format
-    print()
-print("* Execution time:\t", str(endTime - startTime))
-print("* Path cost to goal:\t", node.depth, "moves")
-print("* #Nodes explored:\t", nb_explored)
-print("* Queue size at goal:\t",  remaining_nodes)
+        print("Number of moves: " + str(node.depth))
+        for n in path:
+            print(
+                n.state
+            )  # assuming that the __str__ function of state outputs the correct format
+            print()
+        print("* Execution time:\t", str(endTime - startTime))
+        print("* Path cost to goal:\t", node.depth, "moves")
+        print("* #Nodes explored:\t", nb_explored)
+        print("* Queue size at goal:\t", remaining_nodes)
+    else:
+        instances_path = "instances/"
+        instance_names = [
+            "mine",
+            # "a01",
+            # "a02",
+            # "a03",
+            # "a04",
+            # "a05",
+            # "a06",
+            # "a07",
+            # "a08",
+            # "a09",
+            # "a10",
+            # "a11",
+        ]
+
+        for instance in [instances_path + name for name in instance_names]:
+            grid_init, grid_goal = readInstanceFile(instance)
+            init_state = State(grid_init)
+            problem = Blocks(init_state)
+            print(init_state, "\n" * 2)
+            for _, new_state in problem.successor(init_state):
+                print(new_state)
